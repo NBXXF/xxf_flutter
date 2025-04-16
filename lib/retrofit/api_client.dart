11bbd6fa8c08486f4ebcb370412479176c3fbf70
wuyouxuan <wuyouxuan@thinredline.com.cn>
2025-04-16 10:59:41 +0800

    feat:测试

--- lib/retrofit/api_client.dart
+++ lib/retrofit/api_client.dart
@@ -5,7 +5,7 @@ import '../model/data.dart';
 import 'apis.dart';
 part 'api_client.g.dart';
 @RestApi(baseUrl: "https://gorest.co.in/public-api/")
-@UseCallAdapter(callAdapter)
+///@UseCallAdapter(callAdapter:ResultCallAdapter)
 abstract class ApiClient {
   factory ApiClient(Dio dio, {String baseUrl}) = _ApiClient;
 
