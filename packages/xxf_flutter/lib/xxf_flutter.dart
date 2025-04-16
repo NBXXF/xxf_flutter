55c00894b6279c53ded3dde56129ad5971e513f4
wuyouxuan <wuyouxuan@thinredline.com.cn>
2025-04-16 15:56:54 +0800

    docs:更新文档

--- packages/xxf_flutter/lib/xxf_flutter.dart
+++ packages/xxf_flutter/lib/xxf_flutter.dart
@@ -1,2 +1,6 @@
 export 'package:xxf_json/xxf_json.dart';
 export 'package:xxf_lifecycle/xxf_lifecycle.dart';
+export 'package:xxf_http/xxf_http.dart';
+export 'package:xxf_speed/xxf_speed.dart';
+export 'package:xxf_extensions/xxf_extensions.dart';
+export 'package:xxf_widgets/xxf_widgets.dart';
