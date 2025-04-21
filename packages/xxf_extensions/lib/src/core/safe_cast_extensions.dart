c6e03cc16ecec23318eb6589db95e37f49762040
wuyouxuan <wuyouxuan@thinredline.com.cn>
2025-04-21 10:13:40 +0800

    feat:增加dio logger

--- packages/xxf_extensions/lib/src/core/safe_cast_extensions.dart
+++ packages/xxf_extensions/lib/src/core/safe_cast_extensions.dart
@@ -1,4 +1,7 @@
 ///类型安全转换
 extension SafeCastExtensions on Object? {
   T? asType<T>() => this is T ? this as T : null;
+
+  T asTypeOrDefault<T>(T Function() createFactory) =>
+      this is T ? (this as T) : createFactory();
 }
--- packages/xxf_log/lib/src/http/http_log_interceptor.dart
+++ packages/xxf_log/lib/src/http/http_log_interceptor.dart
@@ -3,11 +3,11 @@ import 'package:xxf_log/src/log_utils.dart';
 
 import 'impl/logging_http_log_interceptor.dart' show LoggingHttpLogInterceptor;
 import 'impl/talker_http_log_interceptor.dart' show TalkerHttpLogInterceptor;
+import '../impl/talker_logger.dart';
 
 /// 网络日志拦截器（基于 TalkerDioLogger 包装）
 class HttpLogInterceptor extends dio.LogInterceptor {
-  LoggingHttpLogInterceptor? _logging;
-  TalkerHttpLogInterceptor? _talker;
+  dio.LogInterceptor? _proxy;
 
   HttpLogInterceptor({
     super.request,
@@ -19,12 +19,20 @@ class HttpLogInterceptor extends dio.LogInterceptor {
   });
 
   dio.LogInterceptor getProxyLogInterceptor() {
-    if (LogUtils.config.logger is TalkerHttpLogInterceptor) {
-      return _talker ??= TalkerHttpLogInterceptor();
+    if (LogUtils.config.logger is TalkerLogger) {
+      _proxy = asTypeOrDefault<TalkerHttpLogInterceptor>(
+        () => TalkerHttpLogInterceptor(),
+      );
     } else {
-      return _logging ??= LoggingHttpLogInterceptor();
+      _proxy = asTypeOrDefault<LoggingHttpLogInterceptor>(
+        () => LoggingHttpLogInterceptor(),
+      );
     }
+    return _proxy!;
   }
+
+  T asTypeOrDefault<T>(T Function() createFactory) =>
+      this is T ? (this as T) : createFactory();
 
   @override
   void onRequest(
