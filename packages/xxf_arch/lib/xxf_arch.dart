acd7513a7a769b784a2810db9bf48d2d722021ad
wuyouxuan <wuyouxuan@thinredline.com.cn>
2025-04-21 21:57:47 +0800

    feat:xxf_arch 0.0.7

--- packages/xxf_arch/lib/xxf_arch.dart
+++ packages/xxf_arch/lib/xxf_arch.dart
@@ -3,15 +3,16 @@ export 'package:flutter_screenutil/flutter_screenutil.dart';
 export 'package:get_it/get_it.dart';
 export 'src/get_it_extensions.dart';
 
+///<<<<<<<<< riverpod <<<<<<<<<<<<<<<<<<
 ///riverpod 关系hooks_riverpod>flutter_riverpod>riverpod
 ///hooks_riverpod=riverpod(dart)+flutter+flutter_hooks
 ///flutter_riverpod=riverpod(dart)+flutter
-/// hooks_riverpod 内部已经包含了这三个包
+///
 export 'package:hooks_riverpod/hooks_riverpod.dart';
 export 'package:flutter_hooks/flutter_hooks.dart';
 export 'package:riverpod/riverpod.dart';
 export 'package:flutter_riverpod/flutter_riverpod.dart';
-
+///>>>>>>>>>> riverpod >>>>>>>>>>>>>>>>
 
 export 'src/adapted_app.dart';
 export 'src/router_manager.dart';
