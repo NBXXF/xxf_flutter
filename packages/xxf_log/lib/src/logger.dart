71a85ba2708b7934cf902a7ab5197cdbc7663a94
wuyouxuan <wuyouxuan@thinredline.com.cn>
2025-04-24 11:59:08 +0800

    docs:各种日志的区别

--- packages/xxf_log/lib/src/logger.dart
+++ packages/xxf_log/lib/src/logger.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/cupertino.dart';
 
+/**
+ * 日志方法	DevTools Logging 视图支持	 说明
+    print()	      ✅       	           简单直接，适用于快速调试。
+    debugPrint()	✅	                 添加了节流机制，防止日志过多导致性能问题。release 模式下仍然会输出日志
+    log()         ✅	                 提供结构化日志信息，适用于需要详细日志的场景。
+    logging 包	  ⚠️	                 默认不显示在 DevTools 中，需要配置输出到 print() 或 log()。
+    talker 包	    ⚠️	                 默认不显示在 DevTools 中，需要配置输出到 print() 或 log()。
+ */
+
 /// 日志接口
 abstract class Logger {
   void logV(String tag, Object log);
