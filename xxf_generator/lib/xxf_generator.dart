713bbc618fd63903ed40ff5f794e4d4f1e37d317
wuyouxuan <wuyouxuan@thinredline.com.cn>
2025-04-25 21:15:03 +0800

    feat:publish generator

--- xxf_generator/lib/xxf_generator.dart
+++ xxf_generator/lib/xxf_generator.dart
@@ -1,2 +1,23 @@
-/// 这里可以导出注解或常用类型
-export 'package:json_annotation/json_annotation.dart';
+import 'dart:async';
+
+import 'package:build/build.dart';
+
+/// 一个简单的构建器工厂，返回一个空构建器
+Builder xxfGeneratorBuilder(BuilderOptions options) {
+  /// 返回一个空的构建器
+  return NoOpBuilder();
+}
+
+/// 一个简单的构建器，实际不做任何工作
+class NoOpBuilder implements Builder {
+  @override
+  FutureOr<void> build(BuildStep buildStep) async {
+    // 这个构建器不执行任何操作
+  }
+
+  @override
+  Map<String, List<String>> get buildExtensions => {
+    // 你可以根据需要定义扩展
+    '.dart': ['.generated.dart'],
+  };
+}
