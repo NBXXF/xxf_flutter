edd27f2deee19715a1865092e0359776a7c08db0
wuyouxuan <wuyouxuan@thinredline.com.cn>
2025-05-11 10:52:25 +0800

    feat:add multi_split_view pod

--- packages/xxf_desktop/lib/xxf_desktop.dart
+++ packages/xxf_desktop/lib/xxf_desktop.dart
@@ -1,7 +1,12 @@
 export 'package:bitsdojo_window/bitsdojo_window.dart';
+
+///系统托盘
 export 'src/system_tray/system_tray_no_menu.dart';
+
+///右键菜单
 export 'package:contextual_menu/contextual_menu.dart';
-
-
 export 'src/contextual_menu/contextual_menu_area.dart';
 export 'src/system_tray/system_tray_extensions.dart';
+
+///分栏视图
+export 'package:multi_split_view/multi_split_view.dart';
