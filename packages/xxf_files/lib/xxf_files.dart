6b05f48e2b9c8cc049d4df95ac1b03c0f6827e7f
wuyouxuan <wuyouxuan@thinredline.com.cn>
2025-05-11 18:41:49 +0800

    feat:add xxf_files api

--- packages/xxf_files/lib/xxf_files.dart
+++ packages/xxf_files/lib/xxf_files.dart
@@ -1,6 +1,7 @@
 ///文件相关
 export 'package:path_provider/path_provider.dart';
-export 'package:path/path.dart';
+///path 有个全局的context 影响组件的使用,widget 内部是buildcontext类型,所以不要导出
+///export 'package:path/path.dart';
 export 'package:file_selector/file_selector.dart';
 
 export 'src/file_system_entity_path_extension.dart';
